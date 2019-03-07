384692a5e645aebcd7fc7dd20f023643b67a0e27
johnniang <johnniang@riseup.net>
2019-03-07 18:20:17 +0800

    Complete getWechatUser and getWechatUserViaSns methods in WechatService

--- src/main/java/me/johnniang/wechat/service/WechatService.java
+++ src/main/java/me/johnniang/wechat/service/WechatService.java
@@ -1,6 +1,7 @@
 package me.johnniang.wechat.service;
 
 import me.johnniang.wechat.support.token.WechatToken;
+import me.johnniang.wechat.support.user.WechatUser;
 import org.springframework.lang.NonNull;
 
 /**
@@ -32,7 +33,26 @@ public interface WechatService {
      * @param signature signature must not be blank
      * @param timestamp timestamp must not be blank
      * @param nonce     nonce must not be blank
-     * @return true if the signature is correct, or false
+     * @return true if the signature is correct, false otherwise
      */
     boolean checkSignature(@NonNull String signature, @NonNull String timestamp, @NonNull String nonce);
+
+    /**
+     * Gets wechat user info.
+     *
+     * @param openid wechat openid must not be blank
+     * @return actual wechat user info
+     */
+    @NonNull
+    WechatUser getWechatUser(@NonNull String openid);
+
+    /**
+     * Gets wechat user info via sns.
+     *
+     * @param openid            wechat openid must not be blank
+     * @param oAuth2AccessToken oauth2 access token
+     * @return actual wechat user info
+     */
+    @NonNull
+    WechatUser getWechatUserViaSns(@NonNull String openid, @NonNull String oAuth2AccessToken);
 }
--- src/main/java/me/johnniang/wechat/service/impl/DefaultWechatServiceImpl.java
+++ src/main/java/me/johnniang/wechat/service/impl/DefaultWechatServiceImpl.java
@@ -1,10 +1,13 @@
 package me.johnniang.wechat.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import me.johnniang.wechat.exception.WechatException;
 import me.johnniang.wechat.properties.WechatProperties;
 import me.johnniang.wechat.service.WechatService;
 import me.johnniang.wechat.support.token.WechatToken;
+import me.johnniang.wechat.support.user.WechatUser;
 import me.johnniang.wechat.util.WechatUtils;
+import org.springframework.lang.NonNull;
 import org.springframework.util.Assert;
 
 import static me.johnniang.wechat.util.WechatUtils.request;
@@ -63,5 +66,45 @@ public class DefaultWechatServiceImpl implements WechatService {
         Assert.hasText(wechatProperties.getValidationToken(), "Wechat validation token must not be blank");
 
         return WechatUtils.checkSignature(signature, timestamp, nonce, wechatProperties.getValidationToken());
+    }
+
+    @Override
+    public WechatUser getWechatUser(String openid) {
+        Assert.hasText(openid, "Openid must not be blank");
+        Assert.hasText(wechatProperties.getUserInfoUrl(), "Wechat get user info url must not be blank");
+
+        return getWechatUserBy(String.format(wechatProperties.getUserInfoUrl(), getWechatTokenString(), openid));
+    }
+
+    @Override
+    public WechatUser getWechatUserViaSns(String openid, String oAuth2AccessToken) {
+        Assert.hasText(openid, "openid must not be blank");
+        Assert.hasText(oAuth2AccessToken, "OAuth2 access token must not be blank");
+        Assert.hasText(wechatProperties.getSnsUserInfoUrl(), "Wechat get user info via sns url must not be blank");
+
+        return getWechatUserBy(String.format(wechatProperties.getSnsUserInfoUrl(), oAuth2AccessToken, openid));
+    }
+
+    /**
+     * Gets wechat user info
+     *
+     * @param requestUserUrl request user url must not be blank
+     * @return actual wechat user
+     */
+    private WechatUser getWechatUserBy(@NonNull String requestUserUrl) {
+        Assert.hasText(requestUserUrl, "Request user url must not be blank");
+
+        // Request for user info
+        WechatUser wechatUser = WechatUtils.request(requestUserUrl, "get", null, WechatUser.class);
+
+        // Check is error response
+        if (!WechatUtils.isResponseSuccessfully(wechatUser)) {
+            // If response is error
+            throw new WechatException("Failed to get wechat user").setData(wechatUser);
+        }
+
+        log.debug("Got wechat user: [{}]", wechatUser);
+
+        return wechatUser;
     }
 }
